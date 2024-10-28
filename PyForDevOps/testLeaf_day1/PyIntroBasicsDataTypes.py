5bb35c0626250588773b3c10da29343ac3f3ed9b
abhinay5993 <abhinaylunawat5993@gmail.com>
2024-10-28 23:50:13 +0530

    Test Leaf : Python3 introductory session - 1 : basics,dataTypes.

---
+++ PyForDevOps/testLeaf_day1/PyIntroBasicsDataTypes.py
@@ -0,0 +1,29 @@
+a=-945.03
+b=945.03
+print("Id value of a : ",id(a));
+print("Id value of b : ",id(b));
+_strPrivateVar="Hey!!.. i am private..";
+print("print the private : ",_strPrivateVar," Type of  : _strPrivateVar ",type(_strPrivateVar));
+p,q,r,s,i=23.44 , 100.333, 10+50j,None,'o';
+print("Value of p : ",p," Type of p : ",type(p));
+print("Value of q : ",q);
+print("Value of r : ",r," Type of r : ",type(r));
+print("Value of s : ",s," Type of s : ",type(s));
+print("Value of i : ",i," Type of s : ",type(i));
+o=p=l=m="Same test"
+print("Value of r : ",o);
+print("Value of p : ",p);
+print("Value of l : ",l);
+print("Value of m : ",m);
+tempStr="Same test";
+print("check the status => p == tempStr :",(p == tempStr));
+
+"""Mutiline operations to be perfromed under the ( ) - Parenthesis."""
+sumNature=(1+2+3+4
+           +5+6+7+8+9);
+print("check the status of mutiline sumasion :",sumNature);
+
+"""Mutiline operations to be perfromed with '\' ."""
+newSumOfSeries=5+9+1+3+4+ \
+2+4;
+print("Mutiline separator by '\\' Sum of numbers :",newSumOfSeries);
